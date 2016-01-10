abdaff4716304df141fd9c33e3147d1f3aeecadb
Paweł Suder <pawel@suder.info>
2016-01-10 12:59:41 +0100

    Add script to prepare generating CRL

---
+++ bin/prepare-to-crl.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##
+# Based on https://jamielinux.com/docs/openssl-certificate-authority/certificate-revocation-lists.html
+##
+
+export __DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export __DIR="$( dirname ${__DIR} )"
+
+CRL_URL=${1}
+
+if [ -z "${CRL_URL}" ]; then
+    echo "Usage: ${0} <crlUrl>"
+    exit -1
+fi
+
+if [ ! -d "${__DIR}/ca/intermediate" ]; then
+    echo "Intermediate CA does not exist!"
+    exit -1
+fi
+
+pushd "${__DIR}/ca/intermediate"
+  echo "Updating intermediate CA configuration..."
+  sed -i 's#\[\s+server\_cert\s+\]#[ server_cert ]\ncrlDistributionPoints = URI:$'${CRL_URL}'#g' openssl.cnf
+  echo "Creating CRL..."
+  openssl ca -config openssl.cnf -gencrl -out crl/intermediate.crl.pem
+  echo "Checking CRL..."
+  openssl crl -in crl/intermediate.crl.pem -noout -text
+popd
